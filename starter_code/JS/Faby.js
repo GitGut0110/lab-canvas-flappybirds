2ef5a5b224e49b7376c217186acf357633d08048
GitGut0110 <jsimms0110@gmail.com>
2018-08-10 18:59:53 +0200

    drawing works....

--- starter_code/JS/Faby.js
+++ starter_code/JS/Faby.js
@@ -1,4 +1,4 @@
-function Player(x,y,w, h, speedX, speedY, g, gSpeed) {
+function Player(x, y, w, h, speedX, speedY, g, gSpeed) {
   this.w = w;
   this.h = h;
   this.speedX = speedX;
@@ -7,16 +7,17 @@ function Player(x,y,w, h, speedX, speedY, g, gSpeed) {
   this.gSpeed = gSpeed;
   this.x = x;
   this.y = y;
-  this.update = function(){
+  this.img = new Image();
+  this.img.src = "images/flappy.png";
+  this.update = function() {
     // img = new Image();
-    // src = "/starter_code/images/flappy.png";
-    console.log ("draw flappy")
-    //  ctx.drawImage(this.img, 200, 200, 300,300)
-    ctx.rect(300,200,200,200)
+    //console.log("draw flappy");
+    ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+    //ctx.rect(300, 200, 200, 200);
     //ctx.fill();
-  }
+  };
 }
 
 Player.prototype.newPos = function() {
   console.log("newPos");
-}
+};
